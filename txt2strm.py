0bf0b272cc1c232ecc534b2b14647a1b3e08c433
Mohammed <nodymoha@yahoo.com>
2022-05-25 01:36:35 -0400

    Update txt2strm.py
    * Create a saving folder
    * Check if the folder is already exists
    * Saving outputs to subdirectory defined by a variable

--- txt2strm.py
+++ txt2strm.py
@@ -1,4 +1,8 @@
 from urllib.parse import unquote
+import os
+
+# SAVING FOLDER
+output_folder = 'STRM'
 
 # read links from txt file and save each links it a separate file
 #
@@ -15,37 +19,42 @@ def get_file_name(url):
     # return file name
     return file_name
 
-def main():
-    # open file
-    infile = open('links.txt', 'r')
-    # read lines from file
-    lines = infile.readlines()
-    # close file
-    infile.close()
-    # create empty list to store lines
-    lines_to_file = []
-    # create empty list to store file names
-    file_names = []
-    # loop through lines
-    for line in lines:
-        # strip new line
-        line = line.strip()
-        # add line to list
-        lines_to_file.append(line)
-    # loop through lines
-    for line in lines_to_file:
-        # create file name
-        file_name = get_file_name(line)
-        # add file name to list
-        file_name = unquote(file_name)
-        file_names.append(file_name)
-    # loop through lines
-    for index in range(len(lines_to_file)):
-        # open file
-        outfile = open(file_names[index] + ".strm", 'w')
-        # write lines to file
-        outfile.write(lines_to_file[index])
-        # close file
-        outfile.close()
 
-main()
+# current working directory
+fileDir = os.path.dirname(os.path.realpath('__file__'))
+# create output directory and check if it exists
+output_dir = os.path.join(fileDir, output_folder)
+if not os.path.exists(output_dir):
+    os.makedirs(output_dir)
+
+# open file
+infile = open('links.txt', 'r')
+# read lines from file
+lines = infile.readlines()
+# close file
+infile.close()
+# create empty list to store lines
+lines_to_file = []
+# create empty list to store file names
+file_names = []
+# loop through lines
+for line in lines:
+    # strip new line
+    line = line.strip()
+    # add line to list
+    lines_to_file.append(line)
+# loop through lines
+for line in lines_to_file:
+    # create file name
+    file_name = get_file_name(line)
+    # add file name to list
+    file_name = unquote(file_name)
+    file_names.append(file_name)
+# loop through lines
+for index in range(len(lines_to_file)):
+    # open file in output directory
+    outfile = open(os.path.join(fileDir, output_folder, file_names[index] + ".strm"), 'w')
+    # write lines to file
+    outfile.write(lines_to_file[index])
+    # close file
+    outfile.close()
